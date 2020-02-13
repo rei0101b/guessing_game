dc60ae3ce903946d9796c3ac935748fe06d2fea5
rei0101b <rei0101b@gmail.com>
2020-02-14 08:57:20 +0900

    input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    let x = 5;
+    let y = 5;
+    println!("x : {}, y: {}", x, y);
+
 }
